5481e2e107ad24343cfcfff4d057e978a0ff3914
WhiteBear <63230518+songbyhyeok@users.noreply.github.com>
2023-12-10 23:01:33 +0900

    feat/#59/posting
    게시판 답글 로직 구현

--- src/main/java/com/hook/hicodingapi/board/service/BoardService.java
+++ src/main/java/com/hook/hicodingapi/board/service/BoardService.java
@@ -3,19 +3,23 @@ package com.hook.hicodingapi.board.service;
 import com.hook.hicodingapi.board.domain.Post;
 import com.hook.hicodingapi.board.domain.repository.BoardCriteriaRepository;
 import com.hook.hicodingapi.board.domain.repository.BoardRepository;
+import com.hook.hicodingapi.board.domain.type.BoardCriteriaConditionType;
 import com.hook.hicodingapi.board.domain.type.BoardType;
 import com.hook.hicodingapi.board.dto.request.PostCreationRequest;
 import com.hook.hicodingapi.board.dto.request.PostUpdateRequest;
-import com.hook.hicodingapi.board.dto.response.PostCreationResponse;
 import com.hook.hicodingapi.board.dto.response.PostReadResponse;
 import com.hook.hicodingapi.common.domain.type.StatusType;
 import com.hook.hicodingapi.common.exception.NotFoundException;
 import com.hook.hicodingapi.member.domain.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import static com.hook.hicodingapi.common.exception.type.ExceptionCode.*;
 
@@ -28,17 +32,41 @@ public class BoardService {
     private final BoardRepository boardRepository;
     private final BoardCriteriaRepository boardCriteriaRepository;
 
-    public List<Post> findAllPosts(final BoardType boardType) {
+    // 답글을 가진 게시글들을 구조 분해시켜 응답 DTO에 담는다.
+    public List<PostReadResponse> convertHierarchicalPostList(final List<Post> postList) {
 
-        final List<Post> postList = boardRepository.findAll();
-        // 없거나, 비어있는 경우
-        if (postList == null || postList.isEmpty()) {
-            throw new NotFoundException(NOT_FOUND_POST_CODE);
-        }
+        // 답글이 없는 게시글(부모)는 List에 담고, 답글 게시글은 부모를 불러 연결(체인)하는 알고리즘
+        List<PostReadResponse> postReadResponseList = new ArrayList<>();
+        Map<Long, PostReadResponse> postReadResponseHashMap = new HashMap<>();
 
-        return postList;
+        postList.forEach(currPost -> {
+            PostReadResponse currPostReadResponse = PostReadResponse.from(currPost);
+            postReadResponseHashMap.put(currPostReadResponse.getNo(), currPostReadResponse);
+
+            if (currPost.getParent() != null) {
+                PostReadResponse parent = postReadResponseHashMap.get(currPost.getParent().getPostNo());
+                if (parent.getChildrenList() == null)
+                    parent.setChildrenList(new ArrayList<>());
+
+                parent.getChildrenList().add(currPostReadResponse);
+            }
+            else
+                postReadResponseList.add(currPostReadResponse);
+        });
+
+        return postReadResponseList;
     }
 
+    @Transactional(readOnly = true)
+    // 게시글 전체 조회
+    public List<Post> findAllPost(BoardType convertedBoardType) {
+        final List<Post> findPostList = boardCriteriaRepository.getPostListByCondition(BoardCriteriaConditionType.FIND_ALL_POST, convertedBoardType).
+                orElseThrow(() -> new NotFoundException(NOT_FOUND_POSTS_CODE));
+
+        return findPostList;
+    }
+
+    @Transactional(readOnly = true)
     // 게시글 조회
     public Post findPost(final BoardType boardType, final Long postNo) {
         final Post findPost = boardRepository.findByPostNoAndBoardTypeAndStatus(postNo, boardType, StatusType.USABLE)
@@ -61,7 +89,8 @@ public class BoardService {
         // parent가 있다는 것은 현재 요청된 게시글이 답글 게시글이라는 것
         final Long parentNo = postCreationReq.getParentNo();
         newPost.setParent(
-                parentNo == null ? null : boardRepository.findById(postCreationReq.getParentNo())
+                parentNo == null ?
+                        null : boardRepository.findById(postCreationReq.getParentNo())
                         .orElse(null)
         );
 
@@ -77,9 +106,14 @@ public class BoardService {
 
     // 게시글 삭제
     public void deletePost(final BoardType boardType, final Long postNo) {
-        //boardRepository.deleteById(postNo);
-
         final Post findPost = findPost(boardType, postNo);
+
+        // 삭제 상태로 전환
         findPost.setStatus(StatusType.DELETED);
+
+        // 서브 자식이 있다면, 제목 앞에 [원글이 삭제된 답글]을 붙이게 한다.
+        for (Post childPost : findPost.getChildrenList()) {
+            childPost.setPostContent("[원글이 삭제된 답글] " + childPost.getPostContent());
+        }
     }
 }
--- src/main/java/com/hook/hicodingapi/common/exception/type/ExceptionCode.java
+++ src/main/java/com/hook/hicodingapi/common/exception/type/ExceptionCode.java
@@ -34,9 +34,11 @@ public enum ExceptionCode {
 
     /* 게시판 */
     NOT_FOUND_POST_CODE(1000, "게시글을 가져올 수 없습니다."),
-    NOT_FOUND_BOARD_TYPE_CODE(1001, "해당 게시판 타입과 일치하지 않습니다."),
+    NOT_FOUND_POSTS_CODE(1001, "게시글들을 가져올 수 없습니다."),
+    NOT_FOUND_BOARD_TYPE_CODE(1002, "해당 게시판 타입과 일치하지 않습니다."),
     NOT_FOUND_WRITER_CODE(1003, "게시글 작성자가 존재하지 않습니다."),
-    NOT_FOUND_CREATION_CODE(1004, "게시글을 생성할 수 없습니다.");
+    NOT_FOUND_CREATION_CODE(1004, "게시글을 생성할 수 없습니다."),
+    FAIL_CONVERT_HIERARCHICAL_CODE(1005, "게시글 목록 구조 분해를 할 수 없습니다.");
 
     private final int code;
     private final String message;
