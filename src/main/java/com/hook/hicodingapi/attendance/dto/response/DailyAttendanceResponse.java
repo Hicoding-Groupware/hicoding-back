ddf964864900a7a14f448e534177c3896e0126b5
HI-SAYA <qnfnsk45@naver.com>
2023-12-14 16:59:15 +0900

    feat/#42/일별 출석부 조회 재수정

--- src/main/java/com/hook/hicodingapi/attendance/dto/response/DailyAttendanceResponse.java
+++ src/main/java/com/hook/hicodingapi/attendance/dto/response/DailyAttendanceResponse.java
@@ -1,14 +1,11 @@
 package com.hook.hicodingapi.attendance.dto.response;
 
-import com.hook.hicodingapi.attendance.domain.Attendance;
 import com.hook.hicodingapi.attendance.domain.type.AttendanceStatusType;
-import com.hook.hicodingapi.record.domain.Record;
 import com.hook.hicodingapi.student.domain.Student;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import java.time.LocalDate;
-import java.util.Objects;
-import java.util.Optional;
+
 
 
 import static lombok.AccessLevel.PRIVATE;
@@ -27,20 +24,7 @@ public class DailyAttendanceResponse {
 
     public static DailyAttendanceResponse from(Student student, LocalDate atdDate) {
 
-        AttendanceStatusType status = null;
-
-        // 첫 번째 등록된 수업만 고려합니다.
-        Record firstRecord = student.getRecordList().stream().findFirst().orElse(null);
-
-        if (firstRecord != null) {
-            // 첫 번째 수업의 출석 목록에서 해당 날짜에 대한 출석 상태를 찾습니다.
-            Optional<Attendance> attendanceOnDate = firstRecord.getCourse().getAttendCosCode().stream()
-                    .filter(attendance -> Objects.equals(attendance.getAtdDate(), atdDate))
-                    .findFirst();
-
-            // 출석 상태가 존재하면 가져옵니다.
-            status = attendanceOnDate.map(Attendance::getAtdStatus).orElse(null);
-        }
+        AttendanceStatusType status = getAttendanceStatusOnDate(student, atdDate);
 
         return new DailyAttendanceResponse(
                 student.getRecordList().get(0).getCourse().getCosCode(),
@@ -52,6 +36,12 @@ public class DailyAttendanceResponse {
                 status
         );
     }
+
+    private static AttendanceStatusType getAttendanceStatusOnDate(Student student, LocalDate atdDate) {
+        return student.getAttendStdCode().stream()
+                .filter(attendance -> atdDate.equals(attendance.getAtdDate()))
+                .findFirst()
+                .map(attendance -> attendance.getAtdStatus())  // 여기서 직접 값을 추출
+                .orElse(null);
+    }
 }
-
-
