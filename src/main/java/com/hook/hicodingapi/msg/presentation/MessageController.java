a3af78817b181e18a0401dc1f18b184dfe725159
HI-SAYA <qnfnsk45@naver.com>
2023-12-20 15:03:28 +0900

    feat/#43/월별 출석부 구현 완료 / 일별 출석부 날짜 조회 수정 완료 / 최종 머지

--- src/main/java/com/hook/hicodingapi/msg/presentation/MessageController.java
+++ src/main/java/com/hook/hicodingapi/msg/presentation/MessageController.java
@@ -24,7 +24,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
-import java.net.URI;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
 import java.time.LocalDate;
